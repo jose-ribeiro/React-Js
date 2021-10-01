f0b5646b8e193a146bffcae254aebe3ec80b0212
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2021-10-01 08:29:35 -0300

    correção nomes das aulas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,19 +10,19 @@ export default () => (
 
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Card titulo="Desafio Aleatorio">
+        <Card titulo="#4 Desafio Aleatorio">
             <NumAleatorio min={1} max={10} />
         </Card>
-        <Card titulo="Desafio Aleatorio">
+        <Card titulo="#3 Fragmento">
             <Fragmento />
         </Card>
-        <Card titulo="Desafio Aleatorio">
+        <Card titulo="#2 Parametros">
           <Parametros
             titulo="Situação do Aluno"
             aluno="Pedro" nota={8.5}
           />
         </Card>
-        <Card titulo="Desafio Aleatorio">
+        <Card titulo="#1 Primeiro">
             <Primeiro></Primeiro>
         </Card>
         
