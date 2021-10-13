89f7fe4f4c720d717d5a963c3a651ca8bb60dc04
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2021-10-13 07:03:02 -0400

    contador dividido em componentes

--- frontend/src/Componentes/contador/Contador.jsx
+++ frontend/src/Componentes/contador/Contador.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from "react"
+import Diplay from "./Diplay";
 import"./Contador.css";
+import Botoes from "./Botoes";
+import PassoForm from "./PassoForm";
 
 
 
@@ -23,26 +26,20 @@ class Contador extends Component {
 
 
     }
-    setPasso = (evento) => {
+    setPasso = (novoPasso) => {
         this.setState({
-            passo: + evento.target.value,
+            passo: novoPasso,
         });
     };
     render() {
         return (
             <div className="Contador">
                 <h2>Contador</h2>
-                <h3>{this.state.numero}</h3>
-                <div>
-                    <label htmlFor="passoInput ">Passo: </label>
-                    <input id="passoInput"type="numeber"
-                    value={this.state.passo} 
-                    onChange={this.setPasso} />
-                </div>
-                <button onClick={this.dec} >-</button>
-                <button onClick={this.inc} >+</button>
+                <Diplay numero={this.state.numero}></Diplay>
+                <PassoForm passo={this.state.passo} setPasso={this.setPasso}/>
 
-
+                <Botoes setInc={this.inc} setDec={this.dec}/>
+                
             </div>
         )
     }
